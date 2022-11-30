f8f841c11125cfa5b5e5283e07b03bdbf62edfdd
isaacrudy <isaac.rudy@gmail.com>
2022-11-30 14:56:29 -0800

    Made main file

--- assignment3.c
+++ assignment3.c
@@ -1,3 +1,24 @@
 //
 // Created by isaac on 11/30/2022.
 //
+#include <stdio.h>
+
+
+
+int main(int argc, char **argv) {
+    int option = 0;
+
+    while (option != 5) {
+        printf("\nPlease select an option:\n");
+        printf("1. load an input file\n"
+               "2. merge holes\n"
+               "3. compact memory\n"
+               "4. print memory view\n"
+               "5. Exit the program\n");
+        scanf("%d", &option);
+
+
+    }
+
+    printf("Exiting Program...");
+}
